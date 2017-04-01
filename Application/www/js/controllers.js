95c6aa6548342ee3356636d1264cab341ac75401
Brian Nelson <emailbriannelson@gmail.com>
2017-04-01 16:37:19 -0500

    Welcome Screen HTML and Firebase Authentication
    Laid out some basic HTML for the welcome screen, began implementing
    Firebase user authentication, but I haven't been able to get it working
    quite yet.

--- Application/www/js/controllers.js
+++ Application/www/js/controllers.js
@@ -1,13 +1,56 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['firebase'])
 
-.controller('WelcomeCtrl', function($scope) {
+/////////////////////////////////////
+//    WELCOME SCREEN CONTROLLER    //
+/////////////////////////////////////
+.controller('WelcomeCtrl', function($scope, $firebase) {
+
+	console.log("Welcome Screen Controller initialized");
+
+	// Fuction for email signup  !!! NOT WORKING !!!
+	$scope.signupEmail = function(){  
+ 
+		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
+ 
+		ref.auth().createUserWithEmailAndPassword($scope.newEmail, $scope.newPassword).catch(function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log("Error creating user:", errorMessage);
+		});	 
+	};
+
+	// Function for email signin  !!! NOT WORKING !!!
+	$scope.loginEmail = function(){
+ 
+		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
+	 
+		ref.authWithPassword({
+	    	email    : $scope.userEmail,
+	    	password : $scope.userPassword
+		}, function(error, authData) {
+	    	if (error) {
+	    		console.log("Login Failed!", error);
+	    	} else {
+	    	console.log("Authenticated successfully with payload:", authData);
+	    	}
+		});
+ 
+	};
 
 })
 
+//////////////////////////////////
+//    CALL SCREEN CONTROLLER    //
+//////////////////////////////////
 .controller('CallCtrl', function($scope) {
 
+	console.log("Call Screen Controller initialized");
+
 })
 
+//////////////////////////////////
+//    HOME SCREEN CONTROLLER    //
+//////////////////////////////////
 .controller('HomeCtrl', function($scope) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -17,8 +60,15 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
+  	console.log("Home Screen Controller initialized");
+
 })
 
+//////////////////////////////////
+//    INFO SCREEN CONTROLLER    //
+//////////////////////////////////
 .controller('InfoCtrl', function($scope) {
+
+	console.log("Info Screen Controller initialized");
 
 })
