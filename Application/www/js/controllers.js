24bb269ba5f9115991fc470592a0960a41e9272f
babramczyk <bjabramczyk@gmail.com>
2017-04-10 16:46:46 -0500

    Refactoring to reflect new tabs and views (per mockups)

--- Application/www/js/controllers.js
+++ Application/www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('starter.controllers', ['firebase'])
 
 /////////////////////////////////////
-//    WELCOME SCREEN CONTROLLER    //
+// WELCOME SCREEN CONTROLLER //
 /////////////////////////////////////
 .controller('WelcomeCtrl', function($scope, $firebase) {
 
 	console.log("Welcome Screen Controller initialized");
 
-	// Fuction for email signup  !!! NOT WORKING !!!
-	$scope.signupEmail = function(){  
+	// Fuction for email signup!!! NOT WORKING !!!
+	$scope.signupEmail = function(){
  
 		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
  
@@ -19,88 +19,94 @@ angular.module('starter.controllers', ['firebase'])
 		});	 
 	};
 
-	// Function for email signin  !!! NOT WORKING !!!
+	// Function for email signin !!! NOT WORKING !!!
 	$scope.loginEmail = function(){
  
 		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
 	 
 		ref.authWithPassword({
-	    	email    : $scope.userEmail,
-	    	password : $scope.userPassword
+		email: $scope.userEmail,
+		password : $scope.userPassword
 		}, function(error, authData) {
-	    	if (error) {
-	    		console.log("Login Failed!", error);
-	    	} else {
-	    	console.log("Authenticated successfully with payload:", authData);
-	    	}
+		if (error) {
+			console.log("Login Failed!", error);
+		} else {
+		console.log("Authenticated successfully with payload:", authData);
+		}
 		});
  
 	};
 
 })
 
-
-//////////////////////////////////
-//    CALL SCREEN CONTROLLER    //
-//////////////////////////////////
-.controller('CallCtrl', function($scope) {
-
-	console.log("Call Screen Controller initialized");
-
-})
-
-
-//////////////////////////////////
-//    HOME SCREEN CONTROLLER    //
-//////////////////////////////////
+	
 .controller('HomeCtrl', function($scope) {
-  // Actual implementations when these functions are set up
-  // $scope.nextElectionDate = getNextElectionDate().toLocaleDateString('en-US',
-//   { month: 'long',
-//     day: 'numeric',
-//     year: 'numeric' });
-  // $scope.pollingPlace = getPollingPlace();
 
-  // Placeholders for now
-  $scope.nextElectionDate = new Date(2017, 4, 3).toLocaleDateString('en-US',
-    { month: 'long',
-      day: 'numeric',
-      year: 'numeric' });
-  $scope.pollingPlace = "342 Langdon Street (Red Gym)";
-
-
-  // Settings Modal
-
-  $ionicModal.fromTemplateUrl('settings-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.settingsModal = modal;
-  });
-
-  $scope.openSettings = function() {
-    $scope.settingsModal.show();
-  };
-  $scope.closeSettings = function() {
-    $scope.settingsModal.hide();
-  };
-  $scope.saveSettings = function() {
-    // TODO: Save text fields to user settings (in db or local storage...)
-
-    $scope.settingsModal.hide();
-  };
-  // $scope.$on('$destroy', function() {
-  //   $scope.modal.remove();
-  // });
-
-  	console.log("Home Screen Controller initialized");
+	console.log("Rep Contact Controller initialized");
 })
 
-//////////////////////////////////
-//    INFO SCREEN CONTROLLER    //
-//////////////////////////////////
-.controller('InfoCtrl', function($scope) {
 
-	console.log("Info Screen Controller initialized");
+.controller('RepContactCtrl', function($scope) {
 
+	console.log("Rep Contact Controller initialized");
+})
+	
+	
+.controller('RepInfoCtrl', function($scope) {
+
+	console.log("Rep Info Controller initialized");
+})
+
+
+.controller('ElectionsCtrl', function($scope) {
+// Actual implementations when these functions are set up
+// $scope.nextElectionDate = getNextElectionDate().toLocaleDateString('en-US',
+//{ month: 'long',
+//day: 'numeric',
+//year: 'numeric' });
+// $scope.pollingPlace = getPollingPlace();
+
+// Placeholders for now
+	$scope.nextElectionDate = new Date(2017, 4, 3).toLocaleDateString('en-US',
+ 		{ month: 'long',
+			day: 'numeric',
+		 	year: 'numeric' });
+	$scope.pollingPlace = "342 Langdon Street (Red Gym)";
+
+
+// Settings Modal
+
+	$ionicModal.fromTemplateUrl('settings-modal.html', {
+ 		scope: $scope,
+ 		animation: 'slide-in-up'
+	}).then(function(modal) {
+ 		$scope.settingsModal = modal;
+	});
+
+	$scope.openSettings = function() {
+	 $scope.settingsModal.show();
+	};
+	$scope.closeSettings = function() {
+	 $scope.settingsModal.hide();
+	};
+	$scope.saveSettings = function() {
+ 		// TODO: Save text fields to user settings (in db or local storage...)
+ 		$scope.settingsModal.hide();
+	};
+// $scope.$on('$destroy', function() {
+//$scope.modal.remove();
+// });
+
+	console.log("Elections Screen Controller initialized");
+})
+
+
+.controller('ActivityCtrl', function($scope) {
+
+	console.log("Activity Screen Controller initialized");
+})
+
+.controller('FeedsCtrl', function($scope) {
+
+	console.log("Feeds Screen Controller initialized");
 })
