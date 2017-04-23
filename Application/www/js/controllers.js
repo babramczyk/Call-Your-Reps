360d51d625b7ba1eb1e2ba825fa91eab2514df1a
babramczyk <bjabramczyk@gmail.com>
2017-04-22 20:33:44 -0500

    Added preliminary welcome view

--- Application/www/js/controllers.js
+++ Application/www/js/controllers.js
@@ -3,40 +3,46 @@ angular.module('starter.controllers', ['firebase'])
 /////////////////////////////////////
 // WELCOME SCREEN CONTROLLER //
 /////////////////////////////////////
-.controller('WelcomeCtrl', function($scope, $firebase) {
+.controller('WelcomeCtrl', function($state, $scope, $firebase) {
 
-	console.log("Welcome Screen Controller initialized");
+	// console.log("Welcome Screen Controller initialized");
+  //
+	// // Fuction for email signup!!! NOT WORKING !!!
+	// $scope.signupEmail = function(){
+  //
+	// 	var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
+  //
+	// 	ref.auth().createUserWithEmailAndPassword($scope.newEmail, $scope.newPassword).catch(function(error) {
+	// 		var errorCode = error.code;
+	// 		var errorMessage = error.message;
+	// 		console.log("Error creating user:", errorMessage);
+	// 	});
+	// };
+  //
+	// // Function for email signin !!! NOT WORKING !!!
+	// $scope.loginEmail = function(){
+  //
+	// 	var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
+	//
+	// 	ref.authWithPassword({
+	// 	email: $scope.userEmail,
+	// 	password : $scope.userPassword
+	// 	}, function(error, authData) {
+	// 	if (error) {
+	// 		console.log("Login Failed!", error);
+	// 	} else {
+	// 	console.log("Authenticated successfully with payload:", authData);
+	// 	}
+	// 	});
+  //
+	// };
 
-	// Fuction for email signup!!! NOT WORKING !!!
-	$scope.signupEmail = function(){
- 
-		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
- 
-		ref.auth().createUserWithEmailAndPassword($scope.newEmail, $scope.newPassword).catch(function(error) {
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			console.log("Error creating user:", errorMessage);
-		});	 
-	};
+	$scope.submit = function() {
+		// TODO: Database person, save user's address from scope into SQLite database
 
-	// Function for email signin !!! NOT WORKING !!!
-	$scope.loginEmail = function(){
- 
-		var ref = new Firebase("https://call-your-reps-b230a.firebaseio.com");
-	 
-		ref.authWithPassword({
-		email: $scope.userEmail,
-		password : $scope.userPassword
-		}, function(error, authData) {
-		if (error) {
-			console.log("Login Failed!", error);
-		} else {
-		console.log("Authenticated successfully with payload:", authData);
-		}
-		});
- 
-	};
-
+		$state.go('tab.home');
+	}
+	
 })
 
 	
